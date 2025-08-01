85d05d112335e9aa0d9c4caa239fe00d8d445bb2
Taylor Kelley <taylorgkelley@outlook.com>
2025-08-01 16:05:44 -0500

    chore: deleting route handlers as all refreshing is done within either server actions or middleware now

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
 import User from './types/User';
 import AuthMiddlewareConfig from './types/AuthMiddlewareConfig';
 import { withAuth } from './middleware';
-import { handleAuth } from './handler';
 import fetchWithAuth from '@/apiClient';
 
-export {
-  handleAuth,
-  withAuth,
-  fetchWithAuth,
-  type AuthMiddlewareConfig,
-  type User,
-};
+export { withAuth, fetchWithAuth, type AuthMiddlewareConfig, type User };
