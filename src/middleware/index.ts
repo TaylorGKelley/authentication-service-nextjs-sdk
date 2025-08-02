4d832f0153ebe2dcc8ae8ef8b5187b8ae4792d06
Taylor Kelley <taylorgkelley@outlook.com>
2025-08-01 19:40:46 -0500

    fix: small syntax bug preventing middleware from properly running for authorized pages

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -6,76 +6,76 @@ import { isExpiredToken } from '@/utils/isExpiredToken';
 import refreshTokens from '@/utils/refreshTokens';
 import type { NextMiddlewareResult } from 'next/dist/server/web/types';
 import {
-  NextResponse,
-  type NextFetchEvent,
-  type NextRequest,
+	NextResponse,
+	type NextFetchEvent,
+	type NextRequest,
 } from 'next/server';
 
 const authMiddleware = async (
-  req: NextRequest,
-  options: AuthMiddlewareConfig,
-  onSuccess: ({ user }: { user: User | null }) => Promise<NextMiddlewareResult>
+	req: NextRequest,
+	options: AuthMiddlewareConfig,
+	onSuccess: ({ user }: { user: User | null }) => Promise<NextMiddlewareResult>
 ) => {
-  try {
-    const url = req.nextUrl.clone();
+	try {
+		const url = req.nextUrl.clone();
 
-    if (Object.keys(options.protectedPaths).includes(url.pathname)) {
-      return await onSuccess({ user: null });
-    }
+		if (!Object.keys(options.protectedPaths).includes(url.pathname)) {
+			return await onSuccess({ user: null });
+		}
 
-    // Check if the user is logged in and has valid permissions
-    const refreshToken = req.cookies.get('refreshToken')?.value;
-    let accessToken = req.cookies.get('accessToken')?.value;
+		// Check if the user is logged in and has valid permissions
+		const refreshToken = req.cookies.get('refreshToken')?.value;
+		let accessToken = req.cookies.get('accessToken')?.value;
 
-    if (!refreshToken) {
-      return NextResponse.redirect(new URL(config.SITE_LOGIN_URL));
-    }
+		if (!refreshToken) {
+			return NextResponse.redirect(new URL(config.SITE_LOGIN_URL));
+		}
 
-    if (!accessToken || isExpiredToken(accessToken)) {
-      await refreshTokens();
-      console.log('refreshed tokens');
-    }
+		if (!accessToken || isExpiredToken(accessToken)) {
+			await refreshTokens();
+			console.log('refreshed tokens');
+		}
 
-    const { user, permissions } = await getPermissions();
+		const { user, permissions } = await getPermissions();
 
-    if (!user) {
-      return NextResponse.redirect(new URL(config.SITE_LOGIN_URL));
-    }
+		if (!user) {
+			return NextResponse.redirect(new URL(config.SITE_LOGIN_URL));
+		}
 
-    // Check if permission matches pages
-    if (
-      !options.protectedPaths[url.pathname].some(
-        (permission) =>
-          permissions.includes(permission) ||
-          permission === config.AUTH_PUBLIC_ROUTE_PERMISSION
-      )
-    ) {
-      // unauthorized
-      return NextResponse.redirect(new URL(config.SITE_UNAUTHORIZED_URL));
-    }
+		// Check if permission matches pages
+		if (
+			!options.protectedPaths[url.pathname].some(
+				(permission) =>
+					permissions.includes(permission) ||
+					permission === config.AUTH_PUBLIC_ROUTE_PERMISSION
+			)
+		) {
+			// unauthorized
+			return NextResponse.redirect(new URL(config.SITE_UNAUTHORIZED_URL));
+		}
 
-    // redirect to login
-    return await onSuccess({ user });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.redirect(new URL(config.SITE_LOGIN_URL));
-  }
+		// redirect to login
+		return await onSuccess({ user });
+	} catch (error) {
+		console.error(error);
+		return NextResponse.redirect(new URL(config.SITE_LOGIN_URL));
+	}
 };
 
 export const withAuth = (
-  middleware: (
-    request: NextRequest & { user: User | null },
-    event: NextFetchEvent
-  ) => NextMiddlewareResult | Promise<NextMiddlewareResult>,
-  options: AuthMiddlewareConfig
+	middleware: (
+		request: NextRequest & { user: User | null },
+		event: NextFetchEvent
+	) => NextMiddlewareResult | Promise<NextMiddlewareResult>,
+	options: AuthMiddlewareConfig
 ) => {
-  return async (
-    ...args: [req: NextRequest & { user: User | null }, event: NextFetchEvent]
-  ) => {
-    const req = args[0];
-    return await authMiddleware(req, options, async ({ user }) => {
-      args[0].user = user;
-      return await middleware(...args);
-    });
-  };
+	return async (
+		...args: [req: NextRequest & { user: User | null }, event: NextFetchEvent]
+	) => {
+		const req = args[0];
+		return await authMiddleware(req, options, async ({ user }) => {
+			args[0].user = user;
+			return await middleware(...args);
+		});
+	};
 };
